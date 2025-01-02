f3abf888f419f878ce3846b84d8602c4a2d1e79f
Alexandre Novaes <ale.oliveira00@gmail.com>
2025-01-02 00:00:30 -0300

    page

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ btn = document.querySelector("#btn-expandir");
 btn.addEventListener("click", function() {
     menu.classList.toggle("expandir");
 });
+
+// menu lateral select
+var menu = document.querySelectorAll(".item-menu")
+
+function selectLink(){
+    menu.forEach((item)=>{
+        item.classList.remove("active")
+    })
+    this.classList.add("active")
+}
+
+menu.forEach((item)=>{
+    item.addEventListener("click", selectLink)
+})
+
+//scroll menu lateral
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
