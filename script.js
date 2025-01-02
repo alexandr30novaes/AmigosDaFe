7aca10a1443e0cccf7a5d0d83b0416a7c0a411f3
Alexandre Novaes <ale.oliveira00@gmail.com>
2025-01-02 00:03:04 -0300

    page

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ btn.addEventListener("click", function() {
     menu.classList.toggle("expandir");
 });
 
-// menu lateral select
-var menu = document.querySelectorAll(".item-menu")
-
-function selectLink(){
-    menu.forEach((item)=>{
-        item.classList.remove("active")
-    })
-    this.classList.add("active")
-}
-
-menu.forEach((item)=>{
-    item.addEventListener("click", selectLink)
-})
 
 //scroll menu lateral
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
