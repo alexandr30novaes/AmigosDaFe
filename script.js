43bf7d203fa3adba3ec70b12850dae8ded9c8660
Alexandre Novaes De Oliveira <ale.oliveira00@gmail.com>
2025-01-21 17:54:46 -0300

    ajuste-page

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 // Animação escroll
-const elements = document.querySelectorAll('.hidden, .anim, .anim-txt'); 
+const elements = document.querySelectorAll('.hidden, .anim, .txt'); 
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
